51014e80b02f2e8aa2f23b8dd0cd3ca93c12497d
EC2 Default User <ec2-user@ip-172-31-14-8.ec2.internal>
2021-01-23 03:16:10 +0000

    クラスの定義

--- TechpitFF/classes/Human.php
+++ TechpitFF/classes/Human.php
@@ -1,3 +1,27 @@
 <?php
 
+class Human{
+    const MAX_HITPOINT = 100;
+    public $name;
+    public $hitPoint =100;
+    public $attackPoint = 20;
+    
+    //メソッド
+    //攻撃メソッド
+    public function doAttack($enemy){
+        echo "『" . $this->name ."』の攻撃！\r\n";
+        echo"『. $enemy->name』に" . $this->attackPoint . "のダメージ！\r\n";
+        $enemy->tookDamege($this->attackPoint);
+    }
+    //ダメージメソッド
+    public function tookDamage($damage){
+        $this->hitPoint -= $damage;
+        //HPが0未満にならないための処理
+        if($this->hitPoint<0){
+            $this->hitPoint=0;
+        }
+    }
+    
+}
+
 ?>
